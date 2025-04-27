fdeca5fb2bc46125d40b65d0bc7beee57e867990
Simon Hörrle <simon.hoerrle@gmail.com>
2025-04-27 21:31:46 +0200

    fix pre-commit issues

--- pytoyoda/const.py
+++ pytoyoda/const.py
@@ -6,7 +6,7 @@ CLIENT_VERSION = "2.14.0"
 # API URLs
 API_BASE_URL = "HTTPS://ctpa-oneapi.tceu-ctp-prd.toyotaconnectedeurope.io"
 ACCESS_TOKEN_URL = (
-    "HTTPS://b2c-login.toyota-europe.com/oauth2/realms/root/realms/tme/access_token"
+    "HTTPS://b2c-login.toyota-europe.com/oauth2/realms/root/realms/tme/access_token"  # noqa : S105
 )
 AUTHENTICATE_URL = "HTTPS://b2c-login.toyota-europe.com/json/realms/root/realms/tme/authenticate?authIndexType=service&authIndexValue=oneapp"
 AUTHORIZE_URL = "HTTPS://b2c-login.toyota-europe.com/oauth2/realms/root/realms/tme/authorize?client_id=oneapp&scope=openid+profile+write&response_type=code&redirect_uri=com.toyota.oneapp:/oauth2Callback&code_challenge=plain&code_challenge_method=plain"
